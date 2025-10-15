fd24c8645dca04d9e19d3b15a54b3d3c0ea81315
Luuk Wubben <luukwubben2@gmail.com>
2025-10-15 12:07:08 +0200

    Train scripts for cfd

---
+++ assignment2/snellius_scripts/cfd_run_both_cond.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --time=01:00:00
+#SBATCH -p gpu_a100
+#SBATCH -N 1
+#SBATCH --tasks-per-node 1
+#SBATCH --gpus=1
+#SBATCH --output=R-%x.%j.out
+
+module purge
+module load 2023
+module load PyTorch/2.1.2-foss-2023a-CUDA-12.1.1 # Includes most dependencies
+
+
+source .venv/bin/activate # Activate virtual environment
+
+pip install -r requirements.txt # Install leftover dependencies
+
+#echo 'Starting new experiment';
+python 	run.py --ch_mults 1 2 4 --hidden_size 256 --lr 1e-5 --predict_frames 20 --history_frames 20 --sigma 0.05 --save --skip_test --condition_on "both" --run_tags "cfd_eval"
+
